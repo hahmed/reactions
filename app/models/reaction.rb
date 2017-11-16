d2a2c31ec27ae32f509056ba1744267311bb8f59
Haroon <haroon.ahmed25@gmail.com>
2017-11-16 16:33:43 +0000

    Validate reactions

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,8 +1,18 @@
 # The reaction to the news
 class Reaction < ApplicationRecord
   belongs_to :article
-  validates :content, presence: true, length: { minimum: 1 }
-
   scope :emojis, -> { where(reaction_type: 'emoji')}
   scope :comments, -> { where(reaction_type: 'text').order(created_at: :desc) }
+
+  validates :content, presence: true, length: { minimum: 1 }
+
+  validates :reaction_type, inclusion: { in: %w(emoji text),
+    message: "%{value} is not a valid option" }
+
+  validates :content, inclusion: { in: %w(thumbs_up thumbs_down lol hooray fear wave prayer),
+    message: "%{value} is not a valid option" }, if: :emoji_reaction?
+
+  def emoji_reaction?
+    reaction_type == 'emoji'
+  end
 end
--- test/models/reaction_test.rb
+++ test/models/reaction_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ReactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save empty reaction" do
+    reaction = Reaction.new
+    assert_not reaction.save
+  end
+
+  test "should not save reaction if type is incorrect" do
+    article = articles(:one)
+    reaction = Reaction.new(article_id: article.id, reaction_type: 'Something')
+    assert_not reaction.save
+  end
+
+  test "should not save reaction if no content" do
+    article = articles(:one)
+    reaction = Reaction.new(article_id: article.id, reaction_type: 'emoji', content: '')
+    assert_not reaction.save
+  end
+
+  test "should not save reaction if emoji content is not valid" do
+    article = articles(:one)
+    reaction = Reaction.new(article_id: article.id, reaction_type: 'emoji', content: 'sleep')
+    assert_not reaction.save
+  end
+
+  test "should save reaction successfully" do
+    article = articles(:one)
+    reaction = Reaction.new(article_id: article.id, reaction_type: 'emoji', content: 'lol')
+    assert reaction.save
+  end
 end
